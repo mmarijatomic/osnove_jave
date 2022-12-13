328db09afae00d1a6d4d83513f3340b3c8314991
Marija Tomic <mmarijatomicc@gmail.com>
2022-12-14 00:24:53 +0100

    Zadatak1

---
+++ src/main/java/d13_12_2022/Zadatak1.java
@@ -0,0 +1,16 @@
+package d13_12_2022;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+//        Napisati program koji stampa verziju softvera. Verzija softvera se sastoji od major, minor i patch brojeva.
+//        Koristan link za verzionisanje softvera. Odstampati na ekranu verziju softvera u formatu:
+//        [MAJOR].[MINOR].[PATCH]
+
+        int MAJOR = 4;
+        int MINOR = 2;
+        int PATCH = 1;
+
+        System.out.println("["+MAJOR+"]"+"."+"["+MINOR+"]"+"."+"["+PATCH+"]");
+
+    }
+}
