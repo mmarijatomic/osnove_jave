7d4a421ab00b8d9039e5f60489b08be27cb50048
Marija Tomic <mmarijatomicc@gmail.com>
2022-12-14 01:04:59 +0100

    Domaci zadatak 3

---
+++ src/main/java/d13_12_2022/Zadatak3.java
@@ -0,0 +1,17 @@
+package d13_12_2022;
+
+public class Zadatak3 {
+    public static void main(String[] args) {
+//        Napisati program koji racuna Z prema formuli: (X - Y) NA KVADRAT. X i Y su celi brojevi.
+
+        int x = 8;
+        int y = 6;
+        int z = (x - y)*(x - y);
+
+        System.out.println("x ="+ x);
+        System.out.println("y ="+ y);
+        System.out.println("z ="+z);
+
+
+    }
+}
