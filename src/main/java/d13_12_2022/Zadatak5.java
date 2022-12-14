2aba9e770a7159a73ce8940948197db3f3ed431a
Marija Tomic <mmarijatomicc@gmail.com>
2022-12-14 01:44:27 +0100

    Domaci zadatak 5

---
+++ src/main/java/d13_12_2022/Zadatak5.java
@@ -0,0 +1,18 @@
+package d13_12_2022;
+
+public class Zadatak5 {
+    public static void main(String[] args) {
+//        Napisati program koji stampa dijagram kao sa slike.
+
+        System.out.println("   x");
+        System.out.println("    ⌃");
+        System.out.println("    |");
+        System.out.println(" 50 |  *   *   *");
+        System.out.println(" 40 |  *   *   *");
+        System.out.println(" 30 |  * * * * *");
+        System.out.println(" 20 |  * * * * *");
+        System.out.println(" 10 |  * * * * * *");
+        System.out.println(" 00 |  * * * * * *");
+        System.out.println("___ | ___________________> y");
+    }
+}
